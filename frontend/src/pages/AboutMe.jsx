75761f561adc757091b760f5676376a9b9e77ddd
dkklimo <dkklimo@gmail.com>
2023-08-28 14:05:37 +0300

    remove clients

--- frontend/src/pages/AboutMe.jsx
+++ frontend/src/pages/AboutMe.jsx
@@ -25,14 +25,14 @@ const About = () => {
 				<AboutCounter />
 			</motion.div>
 
-			<motion.div
+			{/* <motion.div
 				initial={{ opacity: 0 }}
 				animate={{ opacity: 1, delay: 1 }}
 				exit={{ opacity: 0 }}
 				className="container mx-auto"
 			>
 				<AboutClients />
-			</motion.div>
+			</motion.div> */}
 		</AboutMeProvider>
 	);
 };
